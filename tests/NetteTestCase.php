de61567f7ae38cd4c94c8ec4b97298050b45d955
Petr Knap <dev@petrknap.cz>
2016-11-12 13:58:18 +0100

    composer update petrknap/nette-bootstrap

--- tests/NetteTestCase.php
+++ tests/NetteTestCase.php
@@ -7,4 +7,12 @@ use PetrKnap\Nette\Bootstrap\PhpUnit;
 class NetteTestCase extends PhpUnit\NetteTestCase
 {
     const NETTE_BOOTSTRAP_CLASS = NetteBootstrap::class;
+
+    /**
+     * @inheritdoc
+     */
+    protected function clearTempExcludedFiles()
+    {
+        return [".gitignore", ".htaccess"];
+    }
 }
